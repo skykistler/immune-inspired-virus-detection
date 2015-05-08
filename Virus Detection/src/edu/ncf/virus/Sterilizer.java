c337e4fe87797c30ada8bd3496168a2a77f3be35
Sky Kistler <skyler.kistler14@ncf.edu>
2015-05-08 14:14:09 -0400

    sterilizer

--- Virus Detection/src/edu/ncf/virus/Sterilizer.java
+++ Virus Detection/src/edu/ncf/virus/Sterilizer.java
@@ -2,22 +2,18 @@ package edu.ncf.virus;
 
 import java.io.File;
 
-public class Preprocessor {
+public class Sterilizer {
 	File virus_folder = null;
 
-	public Preprocessor(String path) {
+	public Sterilizer(String path) {
 		virus_folder = new File(path);
 		if (!virus_folder.isDirectory())
 			System.out.println("must provide directory");
 	}
 
 	public void process(File f) {
-		// unzip
 		// remove pe header
 		// write out
 	}
 
-	public void unzip() {
-		File[] viruses = virus_folder.listFiles();
-	}
 }
