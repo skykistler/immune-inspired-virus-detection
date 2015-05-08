e8fa8c17e608db1db1cd80c57a906b27b709c941
Sky Kistler <skyler.kistler14@ncf.edu>
2015-05-08 17:34:57 -0400

    sterilized maybe? 20% produced an error

--- Virus Detection/src/edu/ncf/virus/Main.java
+++ Virus Detection/src/edu/ncf/virus/Main.java
@@ -1,7 +1,13 @@
 package edu.ncf.virus;
 
+import java.io.IOException;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Helo viruzssz!");
+	public static void main(String[] args) throws IOException {
+		System.out.println("Helo viruzssz! prepair 2 DIE");
+
+		Sterilizer s = new Sterilizer();
+		s.sterilizeFolder("viruses");
+
 	}
 }
--- Virus Detection/src/edu/ncf/virus/Sterilizer.java
+++ Virus Detection/src/edu/ncf/virus/Sterilizer.java
@@ -1,19 +1,59 @@
 package edu.ncf.virus;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.boris.pecoff4j.PE;
+import org.boris.pecoff4j.SectionTable;
+import org.boris.pecoff4j.io.PEParser;
 
 public class Sterilizer {
 	File virus_folder = null;
 
-	public Sterilizer(String path) {
+	public void sterilizeFolder(String path) throws IOException {
 		virus_folder = new File(path);
 		if (!virus_folder.isDirectory())
 			System.out.println("must provide directory");
+		File sterilized_folder = new File(path + "_sterilized");
+		sterilized_folder.mkdir();
+
+		File[] files = virus_folder.listFiles();
+		double i = 0;
+		double l = files.length;
+		for (File f : files) {
+			if (f.getName().startsWith("."))
+				continue;
+			try {
+				sterilizeVirus(f, sterilized_folder);
+			} catch (Exception e) {
+				System.out.println("Problem sterilizing: " + f.getAbsolutePath());
+				e.printStackTrace();
+				// System.exit(0);
+			}
+			i++;
+			if (i % 100 == 0)
+				System.out.println((100 * i / l) + "% percent complete");
+		}
+		System.out.println("Done.");
 	}
 
-	public void process(File f) {
-		// remove pe header
-		// write out
+	public void sterilizeVirus(File f, File writeto) throws IOException {
+		PE pe = PEParser.parse(f);
+		File sterilized = new File(writeto.getAbsolutePath() + "/" + f.getName());
+		FileOutputStream fos = new FileOutputStream(sterilized);
+		SectionTable st = pe.getSectionTable();
+		int sections = st.getNumberOfSections();
+		for (int i = 0; i < sections; i++) {
+			int characteristics = st.getHeader(i).getCharacteristics();
+			String name = st.getHeader(i).getName().toLowerCase();
+			if ((characteristics & 0xFF) == 0x20 || name.contains("data")) {
+				System.out.println(name);
+				System.out.println(Integer.toHexString(characteristics));
+				fos.write(st.getSection(i).getData());
+			}
+		}
+		fos.flush();
+		fos.close();
 	}
-
 }
