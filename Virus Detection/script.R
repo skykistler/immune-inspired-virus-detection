529ec6ddfb754f7524dae8ba33dee608b4caea3b
Sky Kistler <skyler.kistler14@ncf.edu>
2015-05-09 16:52:57 -0400

    r files

---
+++ Virus Detection/script.R
@@ -0,0 +1,8 @@
+# install.packages('deepnet')
+# install.packages('data.table')
+library('deepnet')
+library('data.table')
+
+virus.list <- list.files(path='viruses_sterilized', full.names=TRUE)
+
+virus.data <- lapply(virus.list, readBin, what="raw")
